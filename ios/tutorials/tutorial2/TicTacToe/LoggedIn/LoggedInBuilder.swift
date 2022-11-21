f398acae10427f8b383bea76b51b79fb734d49a3
bhoon-coding <bhooncoding@gmail.com>
2022-11-21 14:30:18 +0900

    [OffGame] attach OffGame

--- ios/tutorials/tutorial2/TicTacToe/LoggedIn/LoggedInBuilder.swift
+++ ios/tutorials/tutorial2/TicTacToe/LoggedIn/LoggedInBuilder.swift
@@ -12,10 +12,9 @@ protocol LoggedInDependency: Dependency {
     var loggedInViewController: LoggedInViewControllable { get }
 }
 
-final class LoggedInComponent: Component<LoggedInDependency> {
+final class LoggedInComponent: Component<LoggedInDependency>, OffGameDependency {
 
-    // TODO: Make sure to convert the variable into lower-camelcase.
-    fileprivate var LoggedInViewController: LoggedInViewControllable {
+    fileprivate var loggedInViewController: LoggedInViewControllable {
         return dependency.loggedInViewController
     }
 
@@ -38,7 +37,11 @@ final class LoggedInBuilder: Builder<LoggedInDependency>, LoggedInBuildable {
         let component = LoggedInComponent(dependency: dependency)
         let interactor = LoggedInInteractor()
         interactor.listener = listener
+        
+        let offGameBuilder = OffGameBuilder(dependency: component)
         return LoggedInRouter(interactor: interactor,
-                              viewController: component.LoggedInViewController)
+                              viewController: component.loggedInViewController,
+                              offGameBuilder: offGameBuilder
+        )
     }
 }
--- ios/tutorials/tutorial2/TicTacToe/LoggedIn/LoggedInRouter.swift
+++ ios/tutorials/tutorial2/TicTacToe/LoggedIn/LoggedInRouter.swift
@@ -8,24 +8,32 @@
 
 import RIBs
 
-protocol LoggedInInteractable: Interactable {
+protocol LoggedInInteractable: Interactable, OffGameListener {
     var router: LoggedInRouting? { get set }
     var listener: LoggedInListener? { get set }
 }
 
 protocol LoggedInViewControllable: ViewControllable {
-    // TODO: Declare methods the router invokes to manipulate the view hierarchy. Since
-    // this RIB does not own its own view, this protocol is conformed to by one of this
-    // RIB's ancestor RIBs' view.
+    func present(viewController: ViewControllable)
+    func dismiss(viewController: ViewControllable)
 }
 
 final class LoggedInRouter: Router<LoggedInInteractable>, LoggedInRouting {
 
     // TODO: Constructor inject child builder protocols to allow building children.
-    init(interactor: LoggedInInteractable, viewController: LoggedInViewControllable) {
+    init(interactor: LoggedInInteractable,
+         viewController: LoggedInViewControllable,
+         offGameBuilder: OffGameBuildable) {
         self.viewController = viewController
+        self.offGameBuilder = offGameBuilder
         super.init(interactor: interactor)
         interactor.router = self
+    }
+    
+    override func didLoad() {
+        super.didLoad()
+        attachOffGame()
+        
     }
 
     func cleanupViews() {
@@ -36,4 +44,13 @@ final class LoggedInRouter: Router<LoggedInInteractable>, LoggedInRouting {
     // MARK: - Private
 
     private let viewController: LoggedInViewControllable
+    private let offGameBuilder: OffGameBuildable
+    private var currentChild: ViewableRouting?
+    
+    private func attachOffGame() {
+        let offGame = offGameBuilder.build(withListener: interactor)
+        self.currentChild = offGame
+        attachChild(offGame)
+        viewController.present(viewController: offGame.viewControllable)
+    }
 }
